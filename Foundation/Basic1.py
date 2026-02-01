4ba98457675fd0354f62799ef45a39f634873358
Arijit Das <arijitdas.das99@gmail.com>
2026-02-01 10:12:06 +0530

    Updated

---
+++ Foundation/Basic1.py
@@ -0,0 +1,11 @@
+"""
+Docstring for Foundation.Basic1
+
+Everything we use as variable is an object
+"""
+
+name = "xyz"
+num = 12
+
+print(type(name)) # <class 'str'>
+print(type(num))  # <class 'int'>
