e71df65a907173c40791ef9502cd432501ce5906
Arijit Das <arijitdas.das99@gmail.com>
2026-02-01 10:25:08 +0530

    updated

---
+++ Classes&Objects/Example1.py
@@ -0,0 +1,23 @@
+"""
+Docstring for Classes&Objects.Example1
+
+Class is a blueprint of an object
+"""
+
+class Student: # <- Blueprint
+
+    name = "Arijit" 
+
+s1 = Student() # <- Object
+print(s1.name)
+
+
+class Car:
+
+    color = "blue"
+    brand = "BMW"
+
+# Creating an object [c1 is the first object]
+c1 = Car()
+print(c1.color)
+print(c1.brand)
