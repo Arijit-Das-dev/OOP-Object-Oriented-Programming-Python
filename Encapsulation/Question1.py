7248517247e0fb5e17297cd5332bfe047b246035
Arijit Das <arijitdas.das99@gmail.com>
2026-02-03 23:34:33 +0530

    updated

--- Encapsulation/Question1.py
+++ Encapsulation/Question1.py
@@ -1,7 +1,7 @@
 """
 Docstring for Encapsulation.Question1
 
-Create a Cart class:
+Create a ShoppingCart class:
 
 private item list
 
@@ -12,3 +12,27 @@ prevent direct list modification
 📌 Focus: preventing direct data access
 
 """
+
+class ShoppingCart:
+
+    def __init__(self):
+        
+        self.__items = [] # private
+    
+    def addItems(self, *items):
+
+        # adding items in the list
+        self.__items.extend(items)
+    
+    def removeItems(self, *items):
+
+        # removing items from the list
+        self.__items.remove(items)
+    
+    def showItems(self):
+
+        return self.__items
+    
+s = ShoppingCart()
+s.addItems("a", "b" )
+print(s.showItems())
