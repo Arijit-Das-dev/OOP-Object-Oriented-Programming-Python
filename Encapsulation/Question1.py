af6dde6a2709f0bd41dba7060d669d0519e35d16
Arijit Das <arijitdas.das99@gmail.com>
2026-02-03 23:11:47 +0530

    modified

---
+++ Encapsulation/Question1.py
@@ -0,0 +1,14 @@
+"""
+Docstring for Encapsulation.Question1
+
+Create a Cart class:
+
+private item list
+
+methods to add/remove items
+
+prevent direct list modification
+
+📌 Focus: preventing direct data access
+
+"""
