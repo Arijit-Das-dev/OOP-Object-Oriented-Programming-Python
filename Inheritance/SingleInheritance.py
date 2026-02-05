47d105ef93d322c03c92dc1b00b616ee581dda41
Arijit Das <arijitdas.das99@gmail.com>
2026-02-05 21:12:44 +0530

    Syntax

--- Inheritance/SingleInheritance.py
+++ Inheritance/SingleInheritance.py
@@ -1,7 +1,22 @@
 """
 Docstring for Inheritance.SingleInheritance
 
+def => reusing one class inside another class
+
+Example :
+
+One class have it's own quality or method same as another class wants the same or extra quality with the previous
 Structure: 
 
 One Parent -> One child
 """
+
+# Syntax
+
+class Parent:
+
+    pass
+
+class Child(Parent):
+
+    pass
