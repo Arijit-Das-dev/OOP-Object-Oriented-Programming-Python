9a8dc171c2e193df96667dd54f208e18788a9151
Arijit Das <arijitdas.das99@gmail.com>
2026-02-07 10:34:00 +0530

    updated

--- Inheritance/Multiple-Inheritance/Question1.py
+++ Inheritance/Multiple-Inheritance/Question1.py
@@ -3,4 +3,16 @@ Docstring for Inheritance.Multiple-Inheritance.Question1
 
 deff : Where one child class inherits from more than one parent class then it is called Multiple-Inheritance.
        In multiple inheritance parent classes does not need to inherit each other always.
+
+Syntax:
+{
+class Parent1:
+    pass
+    
+class Parent2:
+    pass
+
+class Child(Parent1, Parent2):
+    pass
+} 
 """
